395ca317b5c34949a4ec860ec47d0f0e09a2045f
Diogo Dias <diogocdias@gmail.com>
2017-07-12 15:51:25 +0100

    Implemented Circuit Breaker Pattern on Service A

--- ServiceA/src/main/java/com/example/servicea/ServiceAApplication.java
+++ ServiceA/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -1,19 +1,36 @@
 package com.example.servicea;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;			
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.servicea.ServiceAService;			
+
+@EnableCircuitBreaker
 @RestController
 @SpringBootApplication
 public class ServiceAApplication {
 
-	@RequestMapping(value = "/request")
-	public String request() {
-		return "ServiceA working";
-	}
+	  @Autowired
+	  private ServiceAService serviceAService;
 
+	  @Bean
+	  public RestTemplate rest(RestTemplateBuilder builder) {
+	    return builder.build();
+	  }
+	  
+	  @RequestMapping("/request")
+	  public String request() {
+	    return serviceAService.read();
+	  }
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
 	}
---
+++ ServiceA/src/main/java/com/example/servicea/ServiceAService.java
@@ -0,0 +1,31 @@
+package com.example.servicea;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;			
+
+@Service
+public class ServiceAService {
+
+	  private final RestTemplate restTemplate;
+
+	  public ServiceAService(RestTemplate rest) {
+	    this.restTemplate = rest;
+	  }
+
+	  @HystrixCommand(fallbackMethod = "readFallback")
+	  public String read() {
+	    URI uri = URI.create("http://localhost:8091/read");
+
+	    return this.restTemplate.getForObject(uri, String.class);
+	  }
+
+	  public String readFallback() {
+	    return "ServiceA Fallback method";
+	  }
+
+
+}
